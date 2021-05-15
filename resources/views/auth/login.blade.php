5dd4e5b5a3c1b34d7f90aff939ae0d37cd19b5bb
teguhwin8 <43848500+teguhwin8@users.noreply.github.com>
2021-05-15 20:32:12 +0700

    Tailwind css, nav bottom fixed

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.app')
 @section('content')
+ {{-- 
   <div class="container">
     <div class="row justify-content-center">
       <div class="col-md-8">
@@ -38,4 +39,5 @@
       </div>
     </div>
   </div>
+   --}}
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,8 +21,42 @@
   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 
-<body>
+<body class="bg-gray-100">
   <div id="app">
+    <div class="container mx-auto max-w-sm bg-white min-h-screen">
+      @yield('content')
+      <div class="relative">
+        <div class="block fixed w-full max-w-sm bottom-0">
+          <div class="flex justify-between border-t border-gray-100">
+            <a href="#" class="w-full px-2 flex justify-center items-center hover:bg-gray-100 py-3 text-center mx-auto bg-white">
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+              </svg>
+            </a>
+            <a href="#" class="w-full px-2 flex justify-center items-center hover:bg-gray-100 py-3 text-center bg-white">
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+              </svg>
+            </a>
+            <a href="#" class="w-full px-2 flex justify-center items-center hover:bg-gray-100 py-3 text-center bg-white">
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+              </svg>
+            </a>
+            <a href="#" class="w-full px-2 flex justify-center items-center hover:bg-gray-100 py-3 text-center bg-white">
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z" />
+              </svg>
+            </a>
+            <a href="#" class="w-full px-2 flex justify-center items-center hover:bg-gray-100 py-3 text-center bg-white">
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+              </svg>
+            </a>
+        </div>
+      </div>
+    </div>
+    {{-- 
     <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
       <div class="container">
         <a class="navbar-brand" href="{{ url('/') }}">
@@ -79,6 +113,7 @@
     <main class="py-4">
       @yield('content')
     </main>
+     --}}
   </div>
 </body>
 

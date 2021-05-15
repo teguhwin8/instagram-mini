d7631470bd79aaf91b73a3f2732f10779c060844
teguhwin8 <43848500+teguhwin8@users.noreply.github.com>
2021-05-15 20:39:47 +0700

    Heart icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -45,7 +45,7 @@
             </a>
             <a href="#" class="w-full px-2 flex justify-center items-center hover:bg-gray-100 py-3 text-center bg-white">
               <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z" />
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
               </svg>
             </a>
             <a href="#" class="w-full px-2 flex justify-center items-center hover:bg-gray-100 py-3 text-center bg-white">
